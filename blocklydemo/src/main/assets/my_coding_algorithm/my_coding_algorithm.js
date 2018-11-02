ade7b9140be2546d0f9c24fbb102e42c6e9f08e2
nomco2 <nomco2@naver.com>
2018-11-02 19:18:00 +0900

    1

--- blocklydemo/src/main/assets/my_coding_algorithm/my_coding_algorithm.js
+++ blocklydemo/src/main/assets/my_coding_algorithm/my_coding_algorithm.js
@@ -1,21 +1,4 @@
 
-Blockly.JavaScript['int_data'] = function(block) {
-  var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-
-
-Blockly.JavaScript['control_while_loop'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
 
 Blockly.JavaScript['digital_pin_on'] = function(block) {
   var dropdown_digital_select = block.getFieldValue('digital_select');
@@ -36,29 +19,39 @@ Blockly.JavaScript['equal'] = function(block) {
 
 
 
+//Blockly.JavaScript['control_while_loop'] = function(block) {
+//  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+//  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+//  // TODO: Assemble JavaScript into code variable.
+//  var code = "'for':[{" + value_name + "},{'control_type':1},{" + statements_name + "}]";
+//
+//  return  [code, Blockly.JavaScript.ORDER_NONE];
+//};
+
+
 Blockly.JavaScript['control_while_loop'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_name1 = Blockly.JavaScript.valueToCode(block, 'NAME1', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
   // TODO: Assemble JavaScript into code variable.
-  var code = "'for':[{" + value_name + "},{'control_type':1},{" + statements_name + "}]";
+  var code = "'for':[{" + value_name1 + "},{'control_type':1},{" + statements_name + "}]";
   return code;
 };
 
-Blockly.JavaScript['unequal2'] = function(block) {
-  var value_name1 = Blockly.JavaScript.valueToCode(block, 'NAME1', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_name2 = Blockly.JavaScript.valueToCode(block, 'NAME2', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
+
 
 Blockly.JavaScript['int_data_change'] = function(block) {
-  var variable_name1 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('NAME1'), Blockly.Variables.NAME_TYPE);
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
+  var value_name1 = Blockly.JavaScript.valueToCode(block, 'NAME1', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = variable_name + value_name1;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
+Blockly.JavaScript['number_input'] = function(block) {
+  var number_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = number_name;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
